ea0bd725fed6de56cdff881534d59dfe2e6a2b4a
Luboš Luňák <l.lunak@centrum.cz>
2025-06-19 09:06:36 +0200

    remove sources for old versions
    I'm not updating those anymore anyway, and it's confusing to have copies.

--- 1.5/Source/ModRimWorldRaidExtension/build.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-name=$(ls *.csproj | sed 's/.csproj//')
-rm -f ../../Assemblies/$name.dll 2>/dev/null
-DOTNET_CLI_TELEMETRY_OPTOUT=1 dotnet build $name.csproj /property:Configuration=Release
